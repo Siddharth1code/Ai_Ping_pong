07cfd4182b5b968d0b28f158b97af1225d265b02
Siddharth1code <88075593+Siddharth1code@users.noreply.github.com>
2022-02-22 13:31:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,64 @@
+img="";
+nosex=0;
+nosey=0;
+mariox=325;
+marioy=325;
+gameStatus="";
+/*
+function preload() {
+	img=loadImage('mario05.png');
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+	console.log("the sound");
+}
+
+function setup() {
+	canvas = createCanvas(650,336); 
+	canvas.parent('canvas'); 
+	instializeInSetup(mario);
+	video = createCapture(VIDEO); 
+	video.size(800,400); 
+	video.parent('game_console');
+
+	poseNet=ml5.poseNet(video,modelloaded);
+	poseNet.on('pose',gotPoses);
+}
+
+function draw()
+    {
+	game();
+	background("blue");
+
+	if(nosex <300)
+	{
+		mariox=mariox -1;
+	}
+	if(nosex <300)
+	{
+		mariox=mariox + 1;
+	}
+	if(nosey < 150)
+	{
+		marioy=marioy - 1;
+	}
+	image(img,mariox,marioy,40,70);
+}
+
+function modelloaded() 
+{
+	console.log("modelloaded");
+}
+
+function gotPoses(result)
+{
+	if(result.length >0)
+	{
+		nosex=results[0].pose.nose.x;
+		nosey=results[0].pose.nose.y;
+		console.log("nosex = "+nosex+" ,nosey ="+nosey);
+	}*/
+
 
 /*created by prashant shukla */
 
@@ -23,8 +84,12 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
-}
 
+  video=createCanvas(VIDEO);
+  video.hide();
+  poseNet=ml5.poseNet(video,modelloaded);
+	poseNet.on('pose',gotPoses);
+}
 
 function draw(){
 
@@ -37,7 +102,43 @@ function draw(){
  fill("black");
  stroke("black");
  rect(0,0,20,700);
- 
+
+ if(nosex <300)
+	{
+		mariox=mariox -1;
+	}
+	if(nosex <300)
+	{
+		mariox=mariox + 1;
+	}
+	if(nosey < 150)
+	{
+		marioy=marioy - 1;
+	}
+	image(img,mariox,marioy,40,70);
+}
+
+function modelloaded() 
+{
+	console.log("modelloaded");
+}
+
+function gotPoses(result)
+{
+  if(result.length >0)
+	{
+		nosex=results[0].pose.nose.x;
+		nosey=results[0].pose.nose.y;
+		console.log("nosex = "+nosex+" ,nosey ="+nosey);
+}
+function gotPoses(result)
+{
+	if(result.length >0)
+	{
+		nosex=results[0].pose.nose.x;
+		nosey=results[0].pose.nose.y;
+		console.log("nosex = "+nosex+" ,nosey ="+nosey);
+  }
    //funtion paddleInCanvas call 
    paddleInCanvas();
  
@@ -161,4 +262,5 @@ function paddleInCanvas(){
   if(mouseY < 0){
     mouseY =0;
   }  
+}
 }
